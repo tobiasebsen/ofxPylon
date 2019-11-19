78b1380405c2f6cc63b7806eb12d64459ac1b832
Tobias Ebsen <tobiasebsen@gmail.com>
2019-11-19 09:01:24 +0100

    First commit

---
+++ libs/ofxPylon/src/Camera.cpp
@@ -0,0 +1,200 @@
+#include <pylon/PylonIncludes.h>
+#include <pylon/BaslerUniversalInstantCamera.h>
+
+#include "Camera.h"
+
+
+class ImageEventHandler : public Pylon::CImageEventHandler {
+public:
+	ImageEventHandler(ofxPylon::Camera * c) : parent(c) {}
+
+	void OnImageGrabbed(Pylon::CInstantCamera&, const Pylon::CGrabResultPtr& grabResult) {
+		std::lock_guard<std::mutex> locker(parent->lock);
+
+		try {
+
+			size_t width = grabResult->GetWidth();
+			size_t height = grabResult->GetHeight();
+
+			Pylon::EPixelType pixelType = grabResult->GetPixelType();
+			Pylon::IsMonoImage(pixelType);
+			Pylon::BitDepth(pixelType);
+
+			void * buffer = grabResult->GetBuffer();
+
+			if (pixelType == Pylon::PixelType::PixelType_Mono8) {
+				parent->pixelsFront->allocate(width, height, 1);
+				parent->pixelsFront->setFromPixels(static_cast<unsigned char*>(buffer), width, height, 1);
+				parent->frontGrabbed = true;
+			}
+		}
+		catch (Pylon::GenericException & e) {
+			ofLogError("ofxPylon::Camera") << e.GetDescription();
+		}
+	}
+
+private:
+	ofxPylon::Camera * parent;
+};
+
+
+
+ofxPylon::Camera::~Camera() {
+	close();
+}
+
+std::vector<ofVideoDevice> ofxPylon::Camera::listDevices() const {
+
+	std::vector<ofVideoDevice> devices;
+
+	Pylon::DeviceInfoList_t list;
+	Pylon::CTlFactory::GetInstance().EnumerateDevices(list);
+
+	for (auto & info : list) {
+		ofVideoDevice device;
+		device.id = ofToInt(info.GetDeviceID().c_str());
+		device.deviceName = info.GetModelName();
+		device.hardwareName = info.GetVendorName();
+	}
+
+	return devices;
+}
+
+bool ofxPylon::Camera::setup(int w, int h) {
+
+	Pylon::PylonInitialize();
+
+	try {
+
+		configHandler = shared_ptr<Pylon::CAcquireContinuousConfiguration>(new Pylon::CAcquireContinuousConfiguration);
+		imageHandler = shared_ptr<Pylon::CImageEventHandler>(new ImageEventHandler(this));
+
+		camera = shared_ptr<Pylon::CInstantCamera>(new Pylon::CInstantCamera(Pylon::CTlFactory::GetInstance().CreateFirstDevice()));
+		camera->RegisterImageEventHandler(imageHandler.get(), Pylon::RegistrationMode_ReplaceAll, Pylon::Ownership_ExternalOwnership);
+
+		camera->Open();
+
+		GenApi::INodeMap & nodeMap = camera->GetNodeMap();
+		uint64_t widthMax = Pylon::CIntegerParameter(nodeMap.GetNode("SensorWidth")).GetValue();
+		uint64_t heightMax = Pylon::CIntegerParameter(nodeMap.GetNode("SensorHeight")).GetValue();
+
+		uint64_t binH = floor(widthMax / w);
+		uint64_t binV = floor(heightMax / h);
+
+		Pylon::CIntegerParameter(nodeMap.GetNode("BinningHorizontal")).SetValue(binH);
+		Pylon::CIntegerParameter(nodeMap.GetNode("BinningVertical")).SetValue(binV);
+
+		Pylon::CIntegerParameter(nodeMap.GetNode("Width")).SetValue(w);
+		Pylon::CIntegerParameter(nodeMap.GetNode("Height")).SetValue(h);
+
+		Pylon::CBooleanParameter(nodeMap.GetNode("CenterX")).SetValue(1);
+		Pylon::CBooleanParameter(nodeMap.GetNode("CenterY")).SetValue(1);
+
+		configHandler->OnOpen(*camera);
+
+		camera->StartGrabbing(Pylon::GrabStrategy_OneByOne, Pylon::GrabLoop_ProvidedByInstantCamera);
+	}
+	catch (Pylon::GenericException & e) {
+		ofLogError("ofxPylon::Camera") << e.GetDescription();
+		return false;
+	}
+
+	return true;
+}
+
+bool ofxPylon::Camera::setup(int w, int h, bool useTexture) {
+	this->useTexture = useTexture;
+	return setup(w, h);
+}
+
+float ofxPylon::Camera::getWidth() const {
+	return camera != NULL ? Pylon::CIntegerParameter(camera->GetNodeMap().GetNode("Width")).GetValue() : 0.0f;
+}
+
+float ofxPylon::Camera::getHeight() const {
+	return camera != NULL ? Pylon::CIntegerParameter(camera->GetNodeMap().GetNode("Height")).GetValue() : 0.0f;
+}
+
+bool ofxPylon::Camera::isFrameNew() const {
+	return frameNew;
+}
+
+void ofxPylon::Camera::close() {
+	if (camera) {
+		std::lock_guard<std::mutex> locker(lock);
+
+		camera->StopGrabbing();
+
+		camera->Close();
+		camera.reset();
+
+		imageHandler.reset();
+		configHandler.reset();
+	}
+}
+
+bool ofxPylon::Camera::isInitialized() const {
+	return camera != NULL ? camera->IsPylonDeviceAttached() : false;
+}
+
+bool ofxPylon::Camera::setPixelFormat(ofPixelFormat pixelFormat) {
+	return false;
+}
+
+ofPixelFormat ofxPylon::Camera::getPixelFormat() const {
+	return pixelsBack->getPixelFormat();
+}
+
+ofPixels & ofxPylon::Camera::getPixels() {
+	return *pixelsBack;
+}
+
+const ofPixels & ofxPylon::Camera::getPixels() const {
+	return *pixelsBack;
+}
+
+void ofxPylon::Camera::update() {
+	std::unique_lock<std::mutex> locker(lock, std::defer_lock);
+	frameNew = false;
+
+	if (frontGrabbed && locker.try_lock()) {
+		swap(pixelsBack, pixelsFront);
+		frontGrabbed = false;
+		frameNew = true;
+		locker.unlock();
+
+		if (useTexture) {
+			texture.loadData(getPixels());
+		}
+	}
+}
+
+void ofxPylon::Camera::draw(float x, float y, float w, float h) const {
+	if (texture.isAllocated()) {
+		texture.draw(x, y, w, h);
+	}
+}
+
+std::vector<ofTexture>& ofxPylon::Camera::getTexturePlanes() {
+	return texturePlanes;
+}
+
+const std::vector<ofTexture>& ofxPylon::Camera::getTexturePlanes() const {
+	return texturePlanes;
+}
+
+ofTexture & ofxPylon::Camera::getTexture() {
+	return texture;
+}
+
+const ofTexture & ofxPylon::Camera::getTexture() const {
+	return texture;
+}
+
+void ofxPylon::Camera::setUseTexture(bool bUseTex) {
+	useTexture = bUseTex;
+}
+
+bool ofxPylon::Camera::isUsingTexture() const {
+	return useTexture;
+}
---
+++ libs/ofxPylon/src/Camera.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "ofMain.h"
+
+namespace Pylon {
+	class CInstantCamera;
+	class CConfigurationEventHandler;
+	class CImageEventHandler;
+}
+class ImageEventHandler;
+
+namespace ofxPylon {
+
+	class Camera : public ofBaseVideoGrabber, public ofBaseVideoDraws {
+	public:
+		~Camera();
+
+		// ofBaseVideoGrabber
+		virtual std::vector<ofVideoDevice> listDevices() const;
+		virtual bool setup(int w, int h);
+		virtual bool setup(int w, int h, bool useTexture);
+		virtual float getHeight() const;
+		virtual float getWidth() const;
+
+		// ofBaseVideo
+		virtual bool isFrameNew() const;
+		virtual void close();
+		virtual bool isInitialized() const;
+		virtual bool setPixelFormat(ofPixelFormat pixelFormat);
+		virtual ofPixelFormat getPixelFormat() const;
+
+		// ofBaseHasPixels
+		virtual ofPixels & getPixels();
+		virtual const ofPixels & getPixels() const;
+
+		// ofBaseUpdates
+		virtual void update();
+
+		// ofBaseDraws
+		virtual void draw(float x, float y, float w, float h) const;
+
+		// ofBaseHasTexturePlanes
+		virtual std::vector<ofTexture> & getTexturePlanes();
+		virtual const std::vector<ofTexture> & getTexturePlanes() const;
+
+		// ofBaseHasTexture
+		virtual ofTexture & getTexture();
+		virtual const ofTexture & getTexture() const;
+		virtual void setUseTexture(bool bUseTex);
+		virtual bool isUsingTexture() const;
+
+	protected:
+		friend ImageEventHandler;
+		std::mutex lock;
+
+		ofPixels pixels[2];
+		ofPixels * pixelsFront = &pixels[0];
+		ofPixels * pixelsBack = &pixels[1];
+		bool frontGrabbed = false;
+
+	private:
+		shared_ptr<Pylon::CInstantCamera> camera;
+		shared_ptr<Pylon::CConfigurationEventHandler> configHandler;
+		shared_ptr<Pylon::CImageEventHandler> imageHandler;
+
+		bool frameNew = false;
+
+		ofTexture texture;
+		vector<ofTexture> texturePlanes = { 1 , texture };
+		bool useTexture = true;
+	};
+}
